3b465c3e263135456bcff53b79ada0bac4023f78
farisNassif <g00347032@gmit.ie>
2020-02-04 18:04:45 +0000

    Login HTML improved upon as well as .ts features

--- client/src/app/user/user-login/user-login.component.ts
+++ client/src/app/user/user-login/user-login.component.ts
@@ -12,9 +12,11 @@ export class UserLoginComponent implements OnInit {
   username: String
   loggedInUser: String
   password: String
-  loginConfirmation: String
-  ngOnInit() {
 
+  logged_in: String;
+
+
+  ngOnInit() {
   }
   
   loginUser(event: void, username: string, password: string) {
@@ -26,11 +28,23 @@ export class UserLoginComponent implements OnInit {
     // Cowboy code, honestly no idea why it works
     // If the username/password field isn't blank
     // This just sends username/password to the backend in the format 'username_password'
-    this.userService.loginUser(this.username + "_" + this.password).subscribe(loginConfirmation => (this.loginConfirmation = loginConfirmation))  
+    this.userService.loginUser(this.username + "_" + this.password).subscribe(logged_in => this.checkLogin(this.logged_in = logged_in))
+
     // Just printing to the console the temp logged in user
     console.log("Username: " + this.loggedInUser + " Password: " + password)  
     // 'Clearing' the fields after someone logged in, don't need em anymore - data sent to backend already
     this.username = undefined
-    this.password = undefined 
+    this.password = undefined
+
+  }
+
+  checkLogin(login_result: String) {
+    if (login_result == "true") {
+      // Do login stuff ...
+      console.log("logged in")
+    } else {
+      // Try again
+      console.log("invalid login")
+    }
   }
 }
