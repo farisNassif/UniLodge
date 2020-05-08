bc0feaf6489b8adc04761d0d943444cde9fce68f
farisNassif <g00347032@gmit.ie>
2020-05-08 17:26:18 +0100

    Minimal karma tests included

--- client/src/app/home/home.component.spec.ts
+++ client/src/app/home/home.component.spec.ts
@@ -1,18 +1,55 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { User } from '../user/user'
+import { UserService } from '../user/user.service';
+import { Router } from '@angular/router'
+import { ListingService } from '../listings/listing.service';
+import { Listing } from '../listings/listing';
 
 import { HomeComponent } from './home.component';
+import { debug } from 'util';
+import { url } from 'inspector';
+
+
 
 describe('HomeComponent', () => {
+    let fixture: ComponentFixture<HomeComponent>;
+    let component: HomeComponent;
 
-    let expected = ''
-
-
-    beforeEach(()=> {
-        expected = 'expected'
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                {
+                    provide: Router,
+                    useValue: {
+                       url: '/accommodation/'
+                    } 
+                }
+            ]
+        });
+    
+    let route: ActivatedRoute
+    let userService: UserService
+    let router: Router
+    let listingService: ListingService
+    component = new HomeComponent(route,userService,router,listingService);
     })
 
-    it('checks if hellotest is hellotest', 
-    ()=>expect('hellotest').toBe('hellotest'))
-
+    it('checks that listings are returned', ()=> {
+    spyOn(component, "getListings");
+    component.getListings();
+    expect(component.listings).not.toEqual(null);
+    })
   
+    it('checks that listings navigation works', ()=> {
+        let listing: '/6k5ypfaq0h8';
+        const router = TestBed.get(Router);
+        router.url = '/accommodation/' + '6k5ypfaq0h8';
+
+        spyOn(component, "listingRedirect");
+        component.listingRedirect(listing);
+
+        expect(router.url).toEqual('/accommodation/6k5ypfaq0h8')
+        })
 });
